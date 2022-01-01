336c1d7cdd08dc09ff2f7eba142cad0c94c1336b
Aphananthe <aphananthe42@gmail.com>
2022-01-02 00:37:46 +0900

    Add: integer.py

---
+++ papers/integer.py
@@ -0,0 +1,49 @@
+"""
+整数問題リファレンス
+"""
+
+import sys
+
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+def is_prime(N):
+    """
+    素数判定
+
+    Parameters
+    ----------
+    N : int
+        判定する整数
+    """
+
+    if N < 2: return False
+    i = 2
+    while i * i <= N:
+        if N % i == 0: return False
+        i += 1
+    return True
+
+def eratosthenes(N):
+    """
+    エラトステネスの篩
+    1以上N以下の数が素数かどうかを返す
+
+    Parameters
+    ----------
+    N : int
+        判定する上限
+    """
+
+    table = [True] * (N+1)
+    table[0], table[1] = False, False
+
+    for p in range(2, N+1):
+        if not table[p]:
+            continue
+        q = p * 2
+        while q <= N:
+            table[q] = False
+            q += p
+    return table
