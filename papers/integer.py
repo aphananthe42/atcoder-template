7af1a19ff330634d70ca99aac992e54d51285110
Aphananthe <aphananthe42@gmail.com>
2022-01-03 01:48:57 +0900

    Add: integer methods

--- papers/integer.py
+++ papers/integer.py
@@ -47,3 +47,74 @@ def eratosthenes(N):
             table[q] = False
             q += p
     return table
+
+def calc_divisors(N):
+    """
+    約数列挙
+
+    Parameters
+    ----------
+    N : int
+        判定する整数
+    """
+
+    ans = []
+    for i in range(1, N+1):
+        if i * i > N:
+            break
+        if N % i != 0:
+            continue
+        ans.append(i)
+        if N // i != i:
+            ans.append(N // i)
+    ans.sort()
+    return ans
+
+def prime_factorize(N):
+    """
+    素因数分解
+
+    Parameters
+    ----------
+    N : int
+        分解する整数
+    """
+
+    ans = []
+    for p in range(2, N):
+        if p * p > N:
+            break
+        if N % p != 0:
+            continue
+
+        e = 0
+        while N % p == 0:
+            e += 1
+            N //= p
+        
+        ans.append((p, e))
+    if N != 1:
+        ans.append((N, 1))
+    return ans
+
+def GCD(A, B):
+    """
+    最大公約数
+    """
+    if B == 0: return A
+    else: return GCD(B, A%B)
+
+def LCM(A, B, G):
+    """
+    最小公倍数
+
+    Parameters
+    ----------
+    A : int
+        整数A
+    B : int
+        整数B
+    G : int
+        A, Bの最大公約数
+    """
+    return (A * B) // G
