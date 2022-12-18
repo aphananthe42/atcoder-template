5cc947445573f57b626af861aefe589605965188
Aphananthe <aphananthe42@gmail.com>
2022-12-18 17:20:50 +0900

    Solve 224.b, 225.b

---
+++ pastproblems/abc224/b.py
@@ -0,0 +1,32 @@
+import sys
+from itertools import combinations
+
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+H, W = map(int, input().split())
+A = []
+for i in range(H):
+    a = list(map(int, input().split()))
+    A.append(a)
+
+for i1, i2 in combinations(range(H), 2):
+    for j1, j2 in combinations(range(W), 2):
+        print(i1, i2, j1, j2)
+        if A[i1][j1]+A[i2][j2] <= A[i2][j1]+A[i1][j2]:
+            continue
+        else:
+            print('No')
+            exit()
+
+print('Yes')
+
+# 反省
+# -> 以下のようにやってもちゃんとした2重ループになぜかならない...
+# -> iteratorは変数に格納せずそのまま使うようにする
+I = combinations(range(H), 2)
+J = combinations(range(W), 2)
+for i1, i2 in I:
+    for j1, j2 in J:
+        print(i1, i2, j1, j2)
---
+++ pastproblems/abc225/b.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+N = int(input())
+C = [0]*(N+1)
+
+for i in range(N-1):
+    a, b = map(int, input().split())
+    C[a] += 1
+    C[b] += 1
+
+if (N-1) in C:
+    print('Yes')
+else:
+    print('No')
+
+# 反省
+# -> なぜか有向グラフ的に考えていて詰まっていた
+# -> 無向グラフなのでメモlistの両頂点に辺を記録する
+# -> "Starである"とは(頂点の数-1)個の辺を持つ点を探せばいいので、例えば頂点5個だったら4つの辺を持つ点を探せば良い
