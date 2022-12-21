cb9031685b99728728ace2fbbd3873cb057871d0
Aphananthe <aphananthe42@gmail.com>
2022-12-21 23:55:36 +0900

    Add abc281.b

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ import sys
 def input():
     return sys.stdin.readline()[:-1]
 
+S = input()
+
+if len(S) == 8:
+    isNum = int(S[1:7].zfill(6)) > 99999
+    if S[0].isupper and S[7].isupper:
+        if isNum:
+            print('Yes')
+        else:
+            print('No')
+    else:
+        print('No')
+else:
+    print('No')
