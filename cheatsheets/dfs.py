1355e65e9b0098b9832bb17555db67e154ed9e71
Aphananthe <aphananthe42@gmail.com>
2023-02-05 20:58:10 +0900

    fix: renewal dfs library

--- cheatsheets/dfs.py
+++ cheatsheets/dfs.py
@@ -1,5 +1,5 @@
 """
-深さ優先探索(DFS)基本実装
+深さ優先探索(DFS)関連ライブラリ
 """
 
 import sys
@@ -12,80 +12,30 @@ def input():
     return sys.stdin.readline()[:-1]
 
 
-N = int(input())
-P = list(map(int, input().split()))
+N, M = map(int, input().split())
+G = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    G[a].append(b)
+    G[b].append(a)
 
-# 各頂点の子頂点リストを作る
-chs = [[] for _ in range(N)]
-for v in range(1, N):
-    p = P[v - 1]
-    chs[p].append(v)
+visited = [False] * (N + 1)
 
 
-def dfs_standard(v, chs):
+def dfs(pos, graph, visited):
     """
-    頂点vを根とする部分木を探索する再帰関数
+    深さ優先探索を行う再帰関数
 
     Parameters
     ----------
-    v : int
-        頂点v
-    chs : list
-        頂点vの子頂点のリスト
+    pos: int
+        現在位置
+    graph: list
+        探索を行うグラフ
+    visited: list
+        訪問済みかどうかを記録するリスト
     """
-    print(v, end=" ")
-    for ch in chs[v]:
-        dfs_standard(ch, chs)
-
-
-depth = [0] * N
-
-
-def dfs_depth(v, p, depth, chs):
-    """
-    頂点vを根とする部分木の深さを探索
-
-    Parameters
-    ----------
-    v : int
-        頂点v
-    p : int
-        頂点vの親
-    depth : list
-        頂点vの深さリスト
-    chs : list
-        頂点vの子頂点のリスト
-    """
-    if v == 0:
-        depth[v] = 0
-    else:
-        depth[v] = depth[p] + 1
-
-    for ch in chs[v]:
-        dfs_depth(ch, v, depth, chs)
-
-
-size = [0] * N
-
-
-def dfs_size(v, p, size, chs):
-    """
-    頂点vを根とする部分木の頂点のサイズを探索
-
-    Parameters
-    ----------
-    v: int
-        頂点v
-    p: int
-        頂点vの親
-    size: list
-        頂点vを根とする部分木のサイズリスト
-    chs: list
-        頂点vの子頂点のリスト
-    """
-    for ch in chs[v]:
-        dfs_size(ch, v, size, chs)
-
-    size[v] = 1
-    for ch in chs[v]:
-        size[v] += size[ch]
+    visited[pos] = True
+    for next in graph[pos]:
+        if not visited[next]:
+            dfs(next, graph, visited)
