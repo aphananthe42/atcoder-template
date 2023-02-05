4a4b828a803ccb43a91c574e73397b1ba126d06a
Aphananthe <aphananthe42@gmail.com>
2023-02-05 21:36:16 +0900

    fix: Renewal bfs library

--- cheatsheets/bfs.py
+++ cheatsheets/bfs.py
@@ -10,68 +10,30 @@ def input():
     return sys.stdin.readline()[:-1]
 
 
-################################################
 N, M = map(int, input().split())
-G = [[] for _ in range(N)]
+G = [[] for _ in range(N + 1)]
 for _ in range(M):
-    A, B = map(int, input().split())
-    G[A].append(B)
-    G[B].append(A)
+    a, b = map(int, input().split())
+    G[a].append(b)
+    G[b].append(a)
 
-dist = [-1] * N
-dist[0] = 0
-queue = deque([0])
+shortest_moves = [-1] * (N + 1)
+shortest_moves[1] = 0
 
-
-def bfs_standard():
-    """
-    distに各頂点に到達するための最短手を格納する関数
-    """
-    while queue:
-        v = queue.popleft()
-        for next_v in G[v]:
-            if dist[next_v] != -1:
-                continue
-            dist[next_v] = dist[v] + 1
-            queue.append(next_v)
-
-
-################################################
-# 四方向への移動を表すベクトル, 0: 下、1: 右、2: 上、3: 左
-dxs = [1, 0, -1, 0]
-dys = [0, 1, 0, -1]
-
-H, W = map(int, input().split())
-X0, Y0, X1, Y1 = map(int, input().split())
-S = [input() for _ in range(H)]
-
-# 各マス (x, y) が何手目に探索されたかを格納するlist
-dist = [[-1] * W for _ in range(H)]
 queue = deque()
-
-# マス (X0, Y0) を始点とする
-dist[X0][Y0] = 0
-queue.append((X0, Y0))
+queue.append(1)
 
 
-def bfs_maze():
+def bfs():
     """
-    迷路を幅優先探索し各マスへの最短経路を求める関数
+    shortest_movesに各頂点に到達するための最短手を記録する関数
     """
     while queue:
-        x, y = queue.popleft()
-        # マス (x, y) から 1 手で行けるマスを順に探索
-        for dx, dy in zip(dxs, dys):
-            # 隣接マス
-            x2, y2 = x + dx, y + dy
-            # マス (x2, y2) が盤面外である場合、黒マスである場合はスキップ
-            if x2 < 0 or x2 >= H or y2 < 0 or y2 >= W or S[x2][y2] == "B":
-                continue
-            # マス (x2, y2) が探索済みであれば何もしない
-            if dist[x2][y2] != -1:
-                continue
-            # マス (x2, y2) を探索する
-            dist[x2][y2] = dist[x][y] + 1
-            queue.append((x2, y2))
-    # マス (X1, Y1) の値を答える
-    print(dist[X1][Y1])
+        pos = queue.popleft()
+        for next in G[pos]:
+            if shortest_moves[next] == -1:
+                shortest_moves[next] = shortest_moves[pos] + 1
+                queue.append(next)
+
+    for i in range(1, N + 1):
+        print(shortest_moves[i])
