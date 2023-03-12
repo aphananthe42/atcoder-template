3786b8943e83f1e116bca7757c33ef816685d6c4
Aphananthe <aphananthe42@gmail.com>
2023-03-12 15:36:28 +0900

    add: count_devisors

--- cheatsheets/divisors.py
+++ cheatsheets/divisors.py
@@ -1,4 +1,4 @@
-def calc_divisors(N):
+def enumerate_divisors(N):
     """
     約数列挙
 
@@ -18,3 +18,24 @@ def calc_divisors(N):
             ans.append(N // i)
     ans.sort()
     return ans
+
+
+def count_devisors(N):
+    """
+    約数の個数
+
+    Parameters
+    ----------
+    N: int
+        判定する整数
+    """
+    count = 0
+    for i in range(1, N + 1):
+        if i * i > N:
+            break
+        if N % i != 0:
+            continue
+        count += 1
+        if N // i != i:
+            count += 1
+    return count
